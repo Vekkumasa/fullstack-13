f4720777f9cbbd4692910d3984d41e1c443cbe9c
Vekkumasa <ylirasku@gmail.com>
2022-04-07 14:40:15 +0300

    13.3

---
+++ commands.sql
@@ -0,0 +1,3 @@
+insert into blogs (author, URL, title) values ('Dan Abramov', 'www.daninblogi', 'Writing Resilient Components');
+insert into blogs (author, URL, title) values ('Martin Fowler', 'www.martinblogi', 'Is High Quality Software Worth the Cost?');
+insert into blogs (author, URL, title) values ('Robert C. Martin', 'www.robertinblogi', 'FP vs. OO List Processing');
