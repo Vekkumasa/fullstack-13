bc66c6b88699b6b5ebf728cb8e845cf5a4fbf9d2
Vekkumasa <ylirasku@gmail.com>
2022-04-11 10:11:35 +0300

    13.22

--- controllers/readingList.js
+++ controllers/readingList.js
@@ -2,6 +2,11 @@ const router = require('express').Router()
 const { ReadingList, User } = require('../models')
 const { tokenExtractor } = require('../util/tokenExtractor');
 
+const readingListFinder = async (req, res, next) => {
+  req.readingList = await ReadingList.findByPk(req.params.id)
+  next()
+}
+
 router.post('/', tokenExtractor, async (req, res) => {
   try {
     const user = await User.findByPk(req.decodedToken.id)
@@ -9,6 +14,25 @@ router.post('/', tokenExtractor, async (req, res) => {
     res.json(readingList)
   } catch (error) {
     res.status(404).send({ error: 'Error' });
+  }
+})
+
+router.put('/:id', readingListFinder, tokenExtractor, async (req, res) => {
+  if (req.readingList) {
+    try {
+      const user = await User.findByPk(req.decodedToken.id)
+
+      if (user.id !== req.readingList.userId) {
+        res.status(404).send({ error: 'You can only change read status of your own blogs' });
+      }
+      
+      req.readingList.read = req.body.read
+      req.readingList.save()
+
+      res.json(req.readingList)
+    } catch (error) {
+      res.status(404).send({ error: 'Error at updating readinglist' });
+    }
   }
   
 })
